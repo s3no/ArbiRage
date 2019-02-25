3ad08820d9722995e61e4dfa497c9096f186a837
s3no <47964733+s3no@users.noreply.github.com>
2019-02-25 16:58:44 +0800

    Update ArbiRage.py

--- ArbiRage.py
+++ ArbiRage.py
@@ -11,28 +11,33 @@ def houseOdds():
     tempTwo = 1 / float(teamTwo)
     tempThree = tempOne + tempTwo
     casTake = tempThree * 100
-    w1 = float(wager) / (tempOne / tempTwo + 1)
-    w2 = float(wager) / (tempTwo / tempOne + 1)
+    w1 = round(float(wager) / (tempOne / tempTwo + 1), 2)
+    w2 = round(float(wager) / (tempTwo / tempOne + 1), 2)
 
     #Outputs if the House Wins OR, If you win displays how much to place on each team. 
     if float(casTake) < 100:
        outcomeLabel = Label(window, text="!!!PLACE BET!!!")
        outcomeLabel.grid(row=3, column=1)
-       wageOne = Label(window, text="Team One Bet " + str(w1))
+       wageOne = Label(window, text="Team One Bet: $" + str(w2))
        wageOne.grid(row=4,column=0)
-       wageTwo = Label(window, text="Team Two Bet " + str(w2))
+       wageTwo = Label(window, text="Team Two Bet: $" + str(w1))
        wageTwo.grid(row=5,column=0)
     elif casTake > 100:
        outcomeLabel = Label(window, text="BAIL---House Wins")
        outcomeLabel.grid(row=3, column=1)
+       wageOne = Label(window, text="Team One Bet: $0")
+       wageOne.grid(row=4,column=0)
+       wageTwo = Label(window, text="Team Two Bet: $0" + str(w1))
+       wageTwo.grid(row=5,column=0)
     elif casTake == 100:       
        outcomeLabel = Label(window, text="EVEN SPREAD----100%")
        outcomeLabel.grid(row=3, column=1)
     else:
-       print("ERROR")
+       print("ERROR") 
   
 #Sets up Main Window
 window = Tk()
+
 #Setting up labels and User entrys
 totalWage = Label(window, text="Enter First Teams Odds: ")
 entryWage = Entry(window)
@@ -41,6 +46,7 @@ entryOne = Entry(window)
 oddTwo = Label(window, text="Enter Second Teams Odds: ")
 entryTwo = Entry(window)
 acceptButton = Button(window, text="Calculate", command= houseOdds)
+
 #Places Labels and User entrys into the main window
 totalWage.grid(row=0, column=0)
 entryWage.grid(row=0, column=1)
