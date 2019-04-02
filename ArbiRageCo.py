e1d7c97ebaec2232172518abbc538b1e80158876
s3no <47964733+s3no@users.noreply.github.com>
2019-04-03 01:31:11 +0800

    Update ArbiRageCo.py

--- ArbiRageCo.py
+++ ArbiRageCo.py
@@ -1,46 +1,44 @@
-def houseOdds():
-   wageTotal = input("What Is Your Total Wager Amount?")
-   teamOne = input("Enter First Teams Odds: ")
-   teamTwo = input("Enter Second Teams Odds: ")
+print("------------ARBIRAGE------------")
 
-   tempOne = 1 / float(teamOne)
-   tempTwo = 1 / float(teamTwo)
-   tempThree = tempOne + tempTwo
+def twoWayHouseOdds():
+    #Get Total Bet And Wage
+    wageTotal = input("What Is Your Total Wager Amount?") #eg: 100
+    oddsOne = input("Enter First Teams Odds: ") #eg: 1.3    
+    oddsTwo = input("Enter Second Teams Odds: ") #eg: 10
+    #Storing Total Bet And Wage Complete
 
-   casTake = tempThree * 100
-   print("\n")
+     
+    #Calculations for arbitrage percentage value
+    OddsOneValue = 1 / float(oddsOne)                                        #eg 1 / 1.3 Output: 0.7692307692307692
+    OddsTwoValue = 1 / float(oddsTwo)                                        #eg 1 / 10 Output: 0.1
+    oddsValue = OddsOneValue + OddsTwoValue                                  #eg output: 0.8692307692307692
+    oddsPercent = oddsValue * 100                                            #eg Output:  86.92307692307692
+    
+    #Calculation For How Much To Wage On Each Team Rounded to 2 Decimal Places
+    wageOne = round(float(wageTotal) / (OddsOneValue / OddsTwoValue + 1), 2) #eg: $11.50
+    wageTwo = round(float(wageTotal) / (OddsTwoValue / OddsOneValue + 1), 2) #eg: $88.49
+    #Calculations Compleate				
+	
 
-   if casTake > 100:
-      print("Percentage(above 100% YOU LOOSE!)" + str(casTake) + "%" + "\n")
-   elif casTake < 100:
-      print("Percentage(below 100% YOU WIN!!!!!!):" + str(casTake) + "%" + "\n")
+    #Outputs if the House Wins OR, If you win displays how much to place on each team. 
+    if float(oddsPercent) < 100:
+       #Calculates Your Profit For Both Results
+       profitOne = round(float(wageTwo) * float(oddsOne) - float(wageTotal),2) #eg: Profit: $15.037
+       profitTwo = round(float(wageOne) * float(oddsTwo) - float(wageTotal),2) #eg: Profit: $15.00
+       #Calculation Finished  
+       print("\n!!!PLACE BET!!!\n")
+       print("Team One Bet: $" + str(wageTwo) + "       --Profit: $" + str(profitOne))
+       print("Team Two Bet: $" + str(wageOne) + "       --Profit: $" + str(profitTwo) + "\n")
+    elif casTake > 100:
+       print("BAIL---House Wins\n")
+       print("BAIL: House Wins - " + str(oddsPercent) + "\n")
+       print("BAIL: House Wins - " + str(oddsPercent) + "\n")
+    elif casTake == 100:       
+       print("EVEN SPREAD----100%----BREAK EVEN")
+    else:
+       print("ERROR")
 
-   w1 = round(float(wageTotal) / (tempOne / tempTwo + 1))
-   w2 = round(float(wageTotal) / (tempTwo / tempOne + 1))
-
-   profitOne = float(w2) * float(teamOne)
-   profitTwo = float(w1) * float(teamTwo)
-                        
-   if float(casTake) < 100:
-      print("!!!!!PLACE BET!!!!!" + "\n")
-      print("Your Total Wager: $" + wageTotal)
-      print("Team One Bet " + str(w2) + "       --Profit: $" + str(profitOne))
-      print("Team Two Bet " + str(w1) + "       --Profit: $" + str(profitTwo) + "\n")
-   elif casTake > 100:
-      print("BAIL---House Wins" + "\n")
-   elif casTake == 100:
-      print("EVEN SPREAD----100%" + "\n")
-   else:
-      print("ERROR")
-
-   print("\n" + "\n")
-   print("-----NEW GAME-----" + "\n")
-
-      
+    print("-------NEW-GAME-----------------")
 
 while True:
-   houseOdds()
-
-
-                           
-         
+    twoWayHouseOdds()
