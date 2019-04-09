43811292eb551a1f075278fd97f6adc8edb52b6f
s3no <47964733+s3no@users.noreply.github.com>
2019-04-09 20:06:25 +0800

    Update ArbiRage_OddsBot.py

--- ArbiRage_OddsBot.py
+++ ArbiRage_OddsBot.py
@@ -2,9 +2,10 @@ import json
 import requests
 
 # An api key is emailed to you when you sign up to a plan at (https://the-odds-api.com)
-api_key = '3aa25f97ac0811e92ece986c1981b459'
+api_key = ''
 
-sport_key = ['aussierules_afl', 'americanfootball_nfl', 'mma_mixed_martial_arts', 'rugbyleague_nrl', 'soccer_fifa_world_cup', 'baseball_nba', 'cricket_test_match', 'basketball_euroleague', 'basketball_ncaab', 'rugbyunion_premiership_rugby', 'cricket_odi', 'basketball_mlb']
+# Use 'upcoming' for upcoming and live game odds
+sport_key = ['aussierules_afl', 'americanfootball_nfl', 'mma_mixed_martial_arts', 'soccer_fifa_world_cup', 'baseball_nba', 'cricket_test_match', 'basketball_euroleague', 'basketball_ncaab', 'rugbyunion_premiership_rugby', 'cricket_odi', 'basketball_mlb']
 
 def houseOdds():
     #Gets the total Wager and Odds from user entrys
@@ -26,13 +27,11 @@ def houseOdds():
        profitOne = round(float(w2) * float(teamOne) - float(wager),2)
        profitTwo = round(float(w1) * float(teamTwo) - float(wager),2)
        print("!!!PLACE BET!!!")
-       print(tempDict['teams'][0] + ' ' + tempDict['teams'][1])
-       print(str(tempDict['teams'][0]) + ': ' + str(teamOneHigh))
-       print(str(tempDict['teams'][1]) + ': ' + str(teamTwoHigh))
        print("Team One Bet: $" + str(w2) + "       --Profit: $" + str(profitOne))
        print("Team Two Bet: $" + str(w1) + "       --Profit: $" + str(profitTwo))
+       print()
     elif casTake > 100:
-       print("BAIL: House Wins - " + str(casTake) + " ")
+       print("-----BAIL: House Wins-----" + "\n")
     elif casTake == 100:       
        print("EVEN SPREAD----100%")
     else:
@@ -41,7 +40,7 @@ def houseOdds():
 tempNumb = 0    
 for sports in sport_key:
     odds_response = requests.get('https://api.the-odds-api.com/v3/odds', params={
-    'api_key': 'upcoming',
+    'api_key': api_key,
     'sport': sport_key[tempNumb],
     'region': 'au', #Change as needed with options - uk | us | au
     'mkt': 'h2h' #Change as needed with options - h2h | spreads | totals
@@ -56,24 +55,33 @@ for sports in sport_key:
     # odds_json['data'] contains a list of live and 
     # upcoming events and odds for different bookmakers.
     # Events are ordered by start time (live events are first)
+        print('----------------------------------------------------------------------------')
         print(
-            sport_key[tempNumb]
+            'Successfully got {} events'.format(len(odds_json['data'])),
+            'Hereare the ' + sport_key[tempNumb] + ' events:'
         )
         tempNum = 0
-        print("\n")
         #Getting the game Info
         for matches in odds_json['data']:
             tempDict = odds_json['data'][tempNum]
+            #goes through each game and prints team
+            for teams in tempDict['teams']:
+                print(teams)
             teamOneHigh = float(0.0)
             teamTwoHigh = float(0.0)
-        #for each game it outputs the odds and the site they are on
+            #for each game it outputs the odds and the site they are on
             for x in tempDict['sites']:
                 tempH2hOdds = x['odds']
                 if tempH2hOdds['h2h'][0] >= teamOneHigh:
                     teamOneHigh = float(tempH2hOdds['h2h'][0])
                 if tempH2hOdds['h2h'][0] >= teamTwoHigh:
-                    teamTwoHigh = float(tempH2hOdds['h2h'][1])                 
-            houseOdds()
+                    teamTwoHigh = float(tempH2hOdds['h2h'][1])
+            if teamOneHigh == 0.00:
+                print()
+            elif teamTwoHigh == 0.00:
+                print()
+            else:
+                houseOdds()
             tempNum += 1            
     tempNumb += 1
     
@@ -81,3 +89,4 @@ for sports in sport_key:
 print()
 print('Remaining requests', odds_response.headers['x-requests-remaining'])
 print('Used requests', odds_response.headers['x-requests-used'])
+
